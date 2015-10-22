e4fe8ea01e976a96e9726ddf6e08064ed07440e3
김태우 <xodn4195@gmail.com>
2015-10-22 13:17:56 +0900

    Update sitekey.js

--- test/sitekey.js
+++ test/sitekey.js
@@ -1,4 +1,4 @@
 // recaptcha test sitekey
 // https://developers.google.com/recaptcha/docs/faq#id-like-to-run-automated-tests-with-recaptcha-v2-what-should-i-do
-// replace it with your own sitekey if you want to check your own recaptcha config
+// replace it with your own sitekey if you want to test your own recaptcha config
 window.sitekey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
