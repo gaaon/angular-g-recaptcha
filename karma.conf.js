5bdbb8ad05e19813bafc42bb7013cf6d3b6fa00a
Taewoo Kim <xodn4195+c9@gmail.com>
2015-11-13 02:15:19 +0000

    Add coverage test with karma
    For clear and accurate test specs, add a karma coverage test.

--- karma.conf.js
+++ karma.conf.js
@@ -40,13 +40,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'angular-g-recaptcha.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['nyan'],
+    reporters: ['nyan', 'coverage'],
 
 
     // web server port
